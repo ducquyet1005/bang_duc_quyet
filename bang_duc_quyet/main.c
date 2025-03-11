f0ce8ad187e0f4d5d497cda791f7fcaf85209dc3
ducquyet1005 <bangducquyet1005@gmail.com>
2025-03-11 19:56:03 +0700

    vong lap

--- bang_duc_quyet/main.c
+++ bang_duc_quyet/main.c
@@ -1,5 +1,12 @@
-#include<stdio.h>
-void main()
-{
-	printf("hello world\n");
+#include <stdio.h>
+
+void main() {
+    for (int i = 1; i <= 10; i++) {
+        if (i % 2 == 0) {  
+            printf("2 x %d = %d\n", i, 2 * i);  
+            continue;  
+        }
+        printf("2 x %d = %d\n", i, 2 * i);  
+    }
+    return 0;
 }
